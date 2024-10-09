2f4da167373070a29ba0c6fa12ef7fdb42b24541
ogone4ek2090 <ahmtimur30@gmail.com>
2024-10-09 21:07:22 +0500

    add login

--- backend/authService/main.py
+++ backend/authService/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from database.core import reg_user
+from database.core import reg_user, log_user
 
 app = FastAPI()
 
@@ -7,3 +7,10 @@ app = FastAPI()
 @app.post("/reg")
 async def registration(username, password):
     reg_user(username, password)
+
+
+@app.post("/log")
+async def login(username, password):
+    if log_user():
+        return
+    return
