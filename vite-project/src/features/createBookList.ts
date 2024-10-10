1afb876b1fb8b7f0cbb5fb2e64dcb36be4c330f1
ogone4ek2090 <ahmtimur30@gmail.com>
2024-10-10 20:06:24 +0500

    del viteproject

--- vite-project/src/features/createBookList.ts
+++
@@ -1,9 +0,0 @@
-import { Book } from './../entities/classes/book.ts'
-
-export function createBookList(coverUrls: string[]): Book[]{
-    const booksArrays: Book[] = []
-
-    coverUrls.forEach(cover => booksArrays.push(new Book(cover)))
-
-    return booksArrays
-}
