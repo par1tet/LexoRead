a5e126f05531d4f2f6e71add0f5d9ae2c5d7677a
qruhich <rus;anbazrov485@gmail.com>
2024-10-16 23:29:58 +0300

    feat: bookmarkService

--- backend/bookmark-service/src/bookmark/bookmark.controller.ts
+++ backend/bookmark-service/src/bookmark/bookmark.controller.ts
@@ -1,8 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { BookmarkService } from './bookmark.service'
+import { CreateBookmarkDto } from './dto/create-bookmark.dto'
 
 @Controller('bookmark')
 export class BookmarkController {
-  
+	constructor(private bookmarkService: BookmarkService) {}
 
-  
+	@Post()
+	async create(@Body() bookmarkDto: CreateBookmarkDto) {
+		return await this.bookmarkService.createBookmark(bookmarkDto)
+	}
+
+	@Get()
+	getAll() {
+		return this.bookmarkService.getAllBookmarks()
+	}
 }
--- backend/bookmark-service/src/main.ts
+++ backend/bookmark-service/src/main.ts
@@ -1,11 +1,10 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function start() {
-  const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule)
-  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+	const PORT = process.env.PORT || 5000
+	const app = await NestFactory.create(AppModule)
+	await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
 
 start()
