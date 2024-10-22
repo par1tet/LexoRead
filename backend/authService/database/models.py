2eeb83be59c8faefd04759d20c8c20108e6393f9
ogone4ek2090 <ahmtimur30@gmail.com>
2024-10-22 21:54:21 +0500

    add user model

--- backend/authService/database/models.py
+++ backend/authService/database/models.py
@@ -1,3 +1,10 @@
-from sqlalchemy import ForeignKey, Column, Integer, null
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column
 from database.database import Base
+
+
+class User(Base):
+    id = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username = mapped_column(String(15))
+    email = mapped_column(String)
+    password = mapped_column(String)
