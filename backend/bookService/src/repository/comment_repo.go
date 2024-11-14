8086e260b3eb4d024fbc5daefc50f8cca8661a3e
quthery <chiefkeef7317@gmail.com>
2024-11-14 22:08:00 +0300

    ss

--- backend/bookService/src/repository/comment_repo.go
+++ backend/bookService/src/repository/comment_repo.go
@@ -23,7 +23,7 @@ func (r *commentRepo) CreateComment(comment *models.Comment) error {
 	return r.db.Create(comment).Error
 }
 
-func (r *commentRepo) GetCommentsByBookID(bookID int) ([]models.Comment, error) {
+func (r *commentRepo) GetCommentsByBookID(bookID int) ([]models.Comment, error){
 	var comments []models.Comment
 	err := r.db.Where("book_id = ?", bookID).Find(&comments).Error
 	return comments, err
